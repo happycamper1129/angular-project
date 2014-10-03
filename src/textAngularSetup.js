b2eca4d266380d711ada42a10f1aefa3113806c3
happycamper1129 <happycamper.sw@gmail.com>
2014-10-03 15:52:44 +1300

    fix(formatting) Fix tabbing

--- src/textAngularSetup.js
+++ src/textAngularSetup.js
@@ -14,7 +14,7 @@ angular.module('textAngularSetup', [])
 		['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
 		['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
 		['justifyLeft','justifyCenter','justifyRight','indent','outdent'],
-		['html', 'insertImage', 'insertLink', 'insertVideo']
+		['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
 	],
 	classes: {
 		focussed: "focussed",
@@ -154,6 +154,12 @@ angular.module('textAngularSetup', [])
 	insertLink: {
 		tooltip: 'Insert / edit link',
 		dialogPrompt: "Please enter a URL to insert"
+	},
+	wordcount: {
+		tooltip: 'Display words Count'
+	},
+		charcount: {
+		tooltip: 'Display characters Count'
 	}
 })
 .run(['taRegisterTool', '$window', 'taTranslations', 'taSelection', function(taRegisterTool, $window, taTranslations, taSelection){
@@ -528,7 +534,7 @@ angular.module('textAngularSetup', [])
 					// create the embed link
 					var urlLink = "http://www.youtube.com/embed/" + ids[0].substring(3);
 					// create the HTML
-					var embed = '<img class="ta-insert-video" ta-insert-video="' + urlLink + '" contenteditable="false" src="" allowfullscreen="true" width="300" frameborder="0" height="250"/>';
+					var embed = '<img class="ta-insert-video" src="http://img.youtube.com/vi/' + ids[0].substring(3) + '/maxresdefault.jpg" ta-insert-video="' + urlLink + '" contenteditable="false" src="" allowfullscreen="true" frameborder="0" />';
 					// insert
 					return this.$editor().wrapSelection('insertHTML', embed, true);
 				}
@@ -609,6 +615,50 @@ angular.module('textAngularSetup', [])
 				container.append(buttonGroup);
 				editorScope.showPopover($element);
 			}
+		}
+	});
+	taRegisterTool('wordcount', {
+		display: '<div id="toolbarWC" style="display:block; width:100px;">Words:{{wordcount}}</div>',
+		activeState: function(){ // this fires on keyup
+			if( this.$editor().displayElements)
+			{
+				var textElement = this.$editor().displayElements.text;
+				var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
+
+				// Caculate number of words
+				var sourceTextArray = sourceText.replace(/\s+/g,' ').split(' ');
+				var noOfWords = 0;
+				for (var i = 0; i < sourceTextArray.length; i++) {
+					if (sourceTextArray[i] !== '') {
+						noOfWords++;
+					}
+				}
+
+				//Set current scope
+				this.wordcount = noOfWords;
+				//Set editor scope
+				this.$editor().wordcount = noOfWords;
+			}
+			return false;
+		}
+	});
+	taRegisterTool('charcount', {
+			display: '<div id="toolbarCC" style="display:block; width:120px;">Characters:{{charcount}}</div>',
+			activeState: function(){ // this fires on keyup
+			if( this.$editor().displayElements)
+			{
+				var textElement = this.$editor().displayElements.text;
+				var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
+
+				// Caculate number of chars
+				var noOfChars = sourceText.replace(/^\s+/g,' ').replace(/\s+$/g, ' ').length;
+
+				//Set current scope
+				this.charcount = noOfChars;
+				//Set editor scope
+				this.$editor().charcount = noOfChars;
+			}
+			return false;
 		}
 	});
 }]);
