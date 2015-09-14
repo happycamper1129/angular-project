ef4c57fe482eca804a3e5da9b289cadebd5c0a2e
happycamper1129 <happycamper.sw@gmail.com>
2015-09-14 11:32:04 +1000

    Move setup into main

--- src/textAngularSetup.js
+++ src/textAngularSetup.js
@@ -1,4 +1,14 @@
-angular.module('textAngularSetup', [])
+/*
+@license textAngular
+Author : Austin Anderson
+License : 2013 MIT
+Version 1.3.7
+
+See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
+*/
+
+
+angular.module('textAngularSetup', ['textAngular'])
 // Here we set up the global display defaults, to set your own use a angular $provider#decorator.
 .value('taOptions',  {
 	//////////////////////////////////////////////////////////////////////////////////////
