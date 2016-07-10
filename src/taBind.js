2e214c0441f2de4804a8c5231db8f4e361b43f10
happycamper1129 <happycamper.sw@gmail.com>
2016-07-10 13:08:12 -0600

    Fix(DOM): merged fix from Hike-zzz: fixing issue #616 #1174

--- src/taBind.js
+++ src/taBind.js
@@ -823,12 +823,6 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
 								}
 							}
 							var val = _compileHtml();
-							/* istanbul ignore next: FF specific bug fix */
-							if (val==='<br>') {
-								// on Firefox this comes back sometimes as <br> which is strange, so we remove this here
-								//console.log('*************BAD****');
-								val='';
-							}
 							if(_defaultVal !== '' && val.trim() === ''){
 								_setInnerHTML(_defaultVal);
 								taSelection.setSelectionToElementStart(element.children()[0]);
@@ -1058,12 +1052,6 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
 					element.find(selector).on('click', selectorClickHandler);
 				});
 				element.on('drop', fileDropHandler);
-				element.on('blur', function(){
-					/* istanbul ignore next: webkit fix */
-					if(_browserDetect.webkit) { // detect webkit
-						globalContentEditableBlur = true;
-					}
-				});
 			}
 		}
 	};
